649cd90b029c00b03a8d10fbcd9095a7019d4004
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-06-27 14:55:14 +0700

    InputType of EditText

--- app/src/main/java/com/example/duongtainhan555/maytinhbotui/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/maytinhbotui/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.duongtainhan555.maytinhbotui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edTxt1, edTxt2;
+    TextView txtKetQua;
+    Button btnCong, btnTru, btnNhan, btnChia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edTxt1 = findViewById(R.id.edTxt1);
+        edTxt2 = findViewById(R.id.edTxt2);
+        txtKetQua = findViewById(R.id.txtKetQua);
+        btnCong = findViewById(R.id.btnCong);
+        btnTru = findViewById(R.id.btnTru);
+        btnChia = findViewById(R.id.btnChia);
+        btnNhan = findViewById(R.id.btnNhan);
+        
     }
 }
